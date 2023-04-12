30ddde7554aeea04f63c3852502e669f4b4be584
Mudabbir Hussain Momin <mudabbir.hm@gmail.com>
2023-04-12 12:43:08 +0530

    updated the main file

--- dist/sequelize/index.js
+++ dist/sequelize/index.js
@@ -2,8 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sequelize = void 0;
 const sequelize_1 = require("sequelize");
-const DB_URI = process.env.DATABASE_URL;
-exports.sequelize = new sequelize_1.Sequelize(DB_URI, {
+const DATABASE_NAME = process.env.DATABASE_NAME;
+const DATABASE_USERNAME = process.env.DATABASE_USERNAME;
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
+exports.sequelize = new sequelize_1.Sequelize(DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASSWORD, {
+    host: "localhost",
+    dialect: "postgres",
     pool: {
         max: 5,
         min: 0,
--- src/sequelize/index.ts
+++ src/sequelize/index.ts
@@ -1,15 +1,24 @@
 import { Sequelize } from "sequelize";
 
-const DB_URI = process.env.DATABASE_URL as string;
+const DATABASE_NAME = process.env.DATABASE_NAME as string;
+const DATABASE_USERNAME = process.env.DATABASE_USERNAME as string;
+const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
 
-export const sequelize = new Sequelize(DB_URI, {
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-  define: {
-    freezeTableName: true,
-  },
-});
+export const sequelize = new Sequelize(
+  DATABASE_NAME,
+  DATABASE_USERNAME,
+  DATABASE_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    define: {
+      freezeTableName: true,
+    },
+  }
+);
